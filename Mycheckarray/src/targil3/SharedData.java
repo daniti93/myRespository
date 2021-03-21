fa54ce452fafc442acd8f9c30e900c91eab55f75
daniti93 <danitdoron93@gmail.com>
2021-03-21 14:01:06 +0200

    danit2

--- Mycheckarray/src/targil3/SharedData.java
+++ Mycheckarray/src/targil3/SharedData.java
@@ -1,16 +1,21 @@
 package targil3;
+
+import java.util.ArrayList;
+
 public class SharedData 
 {
-	private int [] array;
+	private ArrayList<Integer> array;
 	private boolean [] winArray;
 	private boolean flag;
 	private final int b;
 	
-	public SharedData(int[] array, int b) {
+	public SharedData(ArrayList<Integer> array, int b) {
 		
 		this.array = array;
 		this.b = b;
 	}
+
+	
 
 	public boolean[] getWinArray() 
 	{
@@ -22,7 +27,7 @@ public class SharedData
 		this.winArray = winArray;
 	}
 
-	public int[] getArray() 
+	public ArrayList<Integer> getArray() 
 	{
 		return array;
 	}
